0a7350c236c6039857ae2a954d6b5c56037194d7
Shawn Her Many Horses <shawnhmh@gmail.com>
2017-08-02 10:03:20 -0700

    Clean up setup script

--- setup.py
+++ setup.py
@@ -3,30 +3,28 @@
 import os
 import shutil
 import subprocess
-import sys
 
 
 AFFIRMATIVE_RESPONSES = ('y', 'yes')
+FILES_WITH_REPLACEMENTS = ['manage.py', 'alpha/urls.py', 'alpha/settings/base.py', 'alpha/wsgi.py']
 
 base_path = os.getcwd()
 base_settings_path = os.path.join(base_path, 'alpha/settings/base.py')
 
-delete_git = raw_input('Allow script to delete old git info? ').lower()
-
+# Ask whether or not to delete old git info
+delete_git = raw_input('Delete .git directory? ').lower()
 if delete_git in AFFIRMATIVE_RESPONSES:
-    #shutil.rmtree(os.path.join(dir_path, '.git'))
-    pass
-else:
-    print 'Must delete git info to proceed. Exiting setup...'
-    sys.exit(1)
+    print 'Deleting .git directory'
+    shutil.rmtree(os.path.join(base_path, '.git'))
 
+# Ask for project name and replace in files
 project_name = raw_input('Project name? (Example: SOME_SITE) ').lower()
 
 settings_src_path = os.path.join(base_path, 'alpha')
 settings_dest_path = os.path.join(base_path, project_name)
 
-for fd in ['manage.py', 'alpha/urls.py', 'alpha/settings/base.py', 'alpha/wsgi.py']:
-    file_path = os.path.join(base_path, fd)
+for path in FILES_WITH_REPLACEMENTS:
+    file_path = os.path.join(base_path, path)
     subprocess.call(['sed', '-i', '-e', 's/ALPHA/{}/g'.format(project_name.upper()), file_path])
     subprocess.call(['sed', '-i', '-e', 's/alpha/{}/g'.format(project_name.lower()), file_path])
 
