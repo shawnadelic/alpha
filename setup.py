66092670524541d6edc606944a82c1edffae1068
Shawn Her Many Horses <shawnhmh@gmail.com>
2017-08-01 19:13:29 -0700

    Use script to replace project-specific values

--- setup.py
+++ setup.py
@@ -2,17 +2,29 @@
 
 import os
 import shutil
+import subprocess
 import sys
 
 
 AFFIRMATIVE_RESPONSES = ('y', 'yes')
 
+base_path = os.getcwd()
+base_settings_path = os.path.join(base_path, 'alpha/settings/base.py')
+
 delete_git = raw_input('Allow script to delete old git info? ').lower()
 
 if delete_git in AFFIRMATIVE_RESPONSES:
-    shutil.rmtree(os.path.join(os.getcwd(), '.git'))
+    #shutil.rmtree(os.path.join(dir_path, '.git'))
+    pass
 else:
     print 'Must delete git info to proceed. Exiting setup...'
     sys.exit(1)
 
-prefix = raw_input('Project prefix? (Example: SOME_SITE) ').upper()
+project_name = raw_input('Project name? (Example: SOME_SITE) ').lower()
+
+settings_src_path = os.path.join(base_path, 'alpha')
+settings_dest_path = os.path.join(base_path, project_name)
+
+subprocess.call(['sed', '-i', '-e', 's/ALPHA/{}/g'.format(project_name.upper()), base_settings_path])
+
+os.rename(settings_src_path, settings_dest_path)
