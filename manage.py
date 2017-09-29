d99a9bf3aad608e211652502d848c0dac4f494cf
Shawn Her Many Horses <shawnhmh@gmail.com>
2017-09-28 17:11:37 -0700

    Manage.py: Add colored traceback

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 from alpha.settings.utils import get_env_variable
+import colored_traceback.always  # noqa
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_env_variable("SETTINGS_MODULE"))
